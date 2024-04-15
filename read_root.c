dc661302079ab9aca8af0197463a0982615fbda2
santy-gg <106451054+santy-gg@users.noreply.github.com>
2024-04-15 01:18:29 -0300

    read_root.c actualizado

--- read_root.c
+++ read_root.c
@@ -2,47 +2,76 @@
 #include <stdlib.h>
 
 typedef struct {
-    unsigned char first_byte;
-    unsigned char start_chs[3];
-    unsigned char partition_type;
-    unsigned char end_chs[3];
-    char starting_cluster[4];
-    char file_size[4];
+	unsigned char first_byte;
+    	unsigned char start_chs[3];
+    	unsigned char partition_type;
+    	unsigned char end_chs[3];
+    	char starting_cluster[4];
+    	unsigned int length_sectors;
 } __attribute((packed)) PartitionTable;
 
 typedef struct {
-    unsigned char jmp[3];
-    char oem[8];
-    unsigned short sector_size;
-	// {...} COMPLETAR
-    unsigned int volume_id;
-    char volume_label[11];
-    char fs_type[8];
-    char boot_code[448];
-    unsigned short boot_sector_signature;
+    	unsigned char jmp[3];
+    	char oem[8];
+    	unsigned short sector_size;
+    	unsigned char sector_cluster;
+	unsigned short reserved_sectors;
+    	unsigned char number_of_fats;
+    	unsigned short root_dir_entries;
+    	unsigned short sector_volumen;
+    	unsigned char descriptor;
+    	unsigned short fat_size_sectors;
+    	unsigned short sector_track;
+    	unsigned short headers;
+    	unsigned int sector_hidden;
+    	unsigned int sector_partition;
+    	unsigned char physical_device;
+    	unsigned char current_header;
+    	unsigned char firm;
+    	unsigned int volume_id;
+    	char volume_label[11];
+    	char fs_type[8]; 
+    	char boot_code[448];
+    	unsigned short boot_sector_signature;
 } __attribute((packed)) Fat12BootSector;
 
 typedef struct {
-	// {...} COMPLETAR
+	unsigned char filename[1];
+    	unsigned char nombre[7];
+    	unsigned char extension[3];
+    	unsigned char atributos[1];
+    	unsigned char reserved;
+    	unsigned char created_time_seconds;
+    	unsigned char created_time_hours_minutes_seconds[2];
+    	unsigned char created_day[2];
+    	unsigned char accessed_day[2];
+    	unsigned char cluster_highbytes_address[2];
+    	unsigned char written_time[2];
+    	unsigned char written_day[2];
+    	unsigned short cluster_lowbytes_address;
+    	unsigned int size_of_file;
 } __attribute((packed)) Fat12Entry;
 
 void print_file_info(Fat12Entry *entry) {
-    switch(entry->filename[0]) {
-    case 0x00:
-        return; // unused entry
-    case ...: // Completar los ...
-        printf("Archivo borrado: [?%.7s.%.3s]\n", // COMPLETAR
+	switch(entry->filename[0]) {
+	case 0x00:
+        	return; // unused entry
+    	case 0xE5: 
+        	printf("Archivo borrado: [?%.7s.%.3s]\n", entry->nombre, entry->extension);
         return;
-    case ...: // Completar los ...
-        printf("Archivo que comienza con 0xE5: [%c%.7s.%.3s]\n", 0xE5, // COMPLETAR 
-        break;
-    case ...: // Completar los ...
-        printf("Directorio: [%.8s.%.3s]\n", // COMPLETAR 
-        break;
-    default:
-        printf("Archivo: [%.8s.%.3s]\n", // COMPLETAR 
-    }
-    
+    	default:
+    	switch(entry->atributos[0]){
+    	case 0x05: 
+        	printf("Archivo que comienza con 0xE5: [%c%.7s.%.3s]\n", 0xE5, entry->nombre, entry->extension);
+        return;
+    	case 0x10: 
+        	printf("Directorio: [%.1s%.8s.%.3s]\n",entry->filename, entry->nombre, entry->extension); 
+        return;
+ 	case 0x20:
+        	printf("Archivo: [%.1s%.8s.%.3s]\n", entry->filename, entry->nombre, entry->extension);
+        return;
+    		} 
+	}
 }
 
 int main() {
@@ -53,6 +82,8 @@ int main() {
     Fat12Entry entry;
    
 	//{...} Completar 
+    fseek(in, 0x1BE, SEEK_SET); //donde comienza la tabla de particiones 
+    fread(pt, sizeof(PartitionTable), 4, in);//leo las entradas
     
     for(i=0; i<4; i++) {        
         if(pt[i].partition_type == 1) {
@@ -67,9 +98,10 @@ int main() {
     }
     
     fseek(in, 0, SEEK_SET);
+    fread(&bs, sizeof(Fat12BootSector), 1, in);
 	//{...} Leo boot sector
     
-    printf("En  0x%X, sector size %d, FAT size %d sectors, %d FATs\n\n", 
+    printf("En  0x%lX, sector size %d, FAT size %d sectors, %d FATs\n\n", 
            ftell(in), bs.sector_size, bs.fat_size_sectors, bs.number_of_fats);
            
     fseek(in, (bs.reserved_sectors-1 + bs.fat_size_sectors * bs.number_of_fats) *
@@ -81,7 +113,7 @@ int main() {
         print_file_info(&entry);
     }
     
-    printf("\nLeido Root directory, ahora en 0x%X\n", ftell(in));
+    printf("\nLeido Root directory, ahora en 0x%lX\n", ftell(in));
     fclose(in);
     return 0;
 }
